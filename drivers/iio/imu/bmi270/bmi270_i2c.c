8abede992a1c4d804b26fd9f13c9b13225d335e3
Antheas Kapenekakis <lkml@antheas.dev>
2025-03-15 23:44:25 +0100

    [TEMPORARY] remove bmi260 matches from bmi270 driver due to sleep issues
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- drivers/iio/imu/bmi270/bmi270_i2c.c
+++ drivers/iio/imu/bmi270/bmi270_i2c.c
@@ -39,7 +39,7 @@ static const struct i2c_device_id bmi270_i2c_id[] = {
 
 static const struct acpi_device_id bmi270_acpi_match[] = {
 	/* GPD Win Mini, Aya Neo AIR Pro, OXP Mini Pro, etc. */
-	{ "BMI0160",  (kernel_ulong_t)&bmi260_chip_info },
+	// { "BMI0160",  (kernel_ulong_t)&bmi260_chip_info },
 	{ }
 };
 
