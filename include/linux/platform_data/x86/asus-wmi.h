ebee992672d0f8578f79ebcece4617198531aa56
Antheas Kapenekakis <lkml@antheas.dev>
2025-03-24 22:01:44 +0100

    platform/x86: asus-wmi: Add support for multiple kbd RGB handlers
    Some devices, such as the Z13 have multiple AURA devices connected
    to them by USB. In addition, they might have a WMI interface for
    RGB. In Windows, Armoury Crate exposes a unified brightness slider
    for all of them, with 3 brightness levels.
    
    Therefore, to be synergistic in Linux, and support existing tooling
    such as UPower, allow adding listeners to the RGB device of the WMI
    interface. If WMI does not exist, lazy initialize the interface.
    
    Reviewed-by: Luke D. Jones <luke@ljones.dev>
    Tested-by: Luke D. Jones <luke@ljones.dev>
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- include/linux/platform_data/x86/asus-wmi.h
+++ include/linux/platform_data/x86/asus-wmi.h
@@ -157,13 +157,29 @@
 #define ASUS_WMI_DSTS_MAX_BRIGTH_MASK	0x0000FF00
 #define ASUS_WMI_DSTS_LIGHTBAR_MASK	0x0000000F
 
+struct asus_hid_listener {
+	struct list_head list;
+	void (*brightness_set)(struct asus_hid_listener *listener, int brightness);
+};
+
 #if IS_REACHABLE(CONFIG_ASUS_WMI)
 int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1, u32 *retval);
+
+int asus_hid_register_listener(struct asus_hid_listener *cdev);
+void asus_hid_unregister_listener(struct asus_hid_listener *cdev);
 #else
 static inline int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1,
 					   u32 *retval)
 {
 	return -ENODEV;
+}
+
+static inline int asus_hid_register_listener(struct asus_hid_listener *bdev)
+{
+	return -ENODEV;
+}
+static inline void asus_hid_unregister_listener(struct asus_hid_listener *bdev)
+{
 }
 #endif
 
